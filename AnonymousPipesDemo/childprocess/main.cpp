794056edb3dac7f1674ca0e9f8492251e4f97bae
yangkemin <iherewaitfor@163.com>
2023-07-19 16:37:49 +0800

    update AnonymousPipesDemo/childprocess/main.cpp. get pipe handle from commandline

--- AnonymousPipesDemo/childprocess/main.cpp
+++ AnonymousPipesDemo/childprocess/main.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <stdio.h>
+#include <string>
 
 #define BUFSIZE 4096 
  
@@ -11,19 +12,45 @@ int main(int argc, char** argv)
    BOOL bSuccess; 
    
  
-   hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
-   hStdin = GetStdHandle(STD_INPUT_HANDLE); 
-   if ( 
-       (hStdout == INVALID_HANDLE_VALUE) || 
-       (hStdin == INVALID_HANDLE_VALUE) 
-      ) 
-      ExitProcess(1); 
+   //hStdout = GetStdHandle(STD_OUTPUT_HANDLE); 
+   //hStdin = GetStdHandle(STD_INPUT_HANDLE); 
+   //if ( 
+   //    (hStdout == INVALID_HANDLE_VALUE) || 
+   //    (hStdin == INVALID_HANDLE_VALUE) 
+   //   ) 
+   //   ExitProcess(1); 
+
    LPSTR pStr = GetCommandLine();
  
-   
-   // Send something to this process's stdout using printf.
-   printf("\n ** This is a message from the child process.%s %s** \n", pStr, argv[2]);
+   if (argc < 3) {
+       return 1;
+   }
+   std::string strWriteHandle;
+   strWriteHandle.assign(argv[1], strlen(argv[1])); // writeHandle=284 
 
+   std::string strReadHandle = argv[2];             // readHandle=288
+
+   size_t offset = strWriteHandle.find_last_of("=");
+   strWriteHandle = strWriteHandle.substr(offset + 1); // 284
+
+   offset = strReadHandle.find_last_of("=");
+   strReadHandle = strReadHandle.substr(offset + 1); // 284
+
+   hStdout = (HANDLE)atoi(strWriteHandle.c_str());
+   hStdin = (HANDLE)atoi(strReadHandle.c_str());
+   if (
+       (hStdout == INVALID_HANDLE_VALUE) ||
+       (hStdin == INVALID_HANDLE_VALUE)
+       )
+       ExitProcess(1);
+
+
+   // Send something to this process's stdout using printf.
+//   printf("\n ** This is a message from the child process.  %s %s %s  ** \n", pStr, strWriteHandle.c_str(), strReadHandle.c_str());
+   
+// WriteFile(hStdout, pStr, sizeof(pStr), &dwWritten, NULL);
+
+   WriteFile(hStdout, "123\n", 4, &dwWritten, NULL);
 
    // This simple algorithm uses the existence of the pipes to control execution.
    // It relies on the pipe buffers to ensure that no data is lost.
